afc58b22aad28bce4942def765fd5b79273a06c1
Jasminkumar Korat <jasminkumarkorat@Jasminkumars-MacBook-Air.local>
2024-06-26 13:48:19 +0530

    e-commerce and authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./Header/Header";
+import Auth from "./Authentication/Auth";
+import Product from "./Product/Product";
+import ForgotPassword from "./Authentication/ForgotPassword/ForgotPassword";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route path="/forgotpassword" exact component={ForgotPassword}/>
+        <Route path="/products" exact component={Product} />
+        <Route path="/" exact component={Auth} />
+      </Switch>
     </div>
   );
 }
